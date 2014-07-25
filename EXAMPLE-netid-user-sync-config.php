9878bf6d984df00969dd21dd018a0718f3551df1
Jonathan Windle <jonathan.windle@bellevuecollege.edu>
2014-07-25 09:33:53 -0700

    Update comments in example config for PHP doc spec

--- EXAMPLE-netid-user-sync-config.php
+++ EXAMPLE-netid-user-sync-config.php
@@ -1,9 +1,8 @@
 <?php
 /*
- * Copy/Rename the file as netid-user-sync-config.php/
- *
+ * Copy/Rename the file as netid-user-sync-config.php
  */
-/*
+
 const MAIN = "main";
 const COMMONS = "commons";
 const EVENTS = "events";
@@ -13,12 +12,26 @@ const DEFAULT_EMAIL_DOMAIN = "@bellevuecollege.edu";
 
 class netidUserSyncConfig
 {
+    /**
+     * DSN to use for connecting to the NetID database
+     *
+     * Example String: dblib:host=dbserver.example.com:1144\MyInstance;dbname=SomeDatabase
+     * More information: http://php.net/manual/en/ref.pdo-dblib.connection.php
+     * @static
+     */
+    public static $dsn = '';
 
-    public static $dsn = ''; // DevMSSQL2008 is configured in /etc/freetds/freetds.conf file
-// Database credentials
+    /**
+     * Database server username
+     * @static
+     */
     public static $netIdDatabaseLogin = "";
-    public static $netIdDatabasePassword = '';
 
+    /**
+     * Database server password
+     * @static
+     */
+    public static $netIdDatabasePassword = '';
 
     public static $sqlColumnEntryID = "EntryID";
     public static $sqlColumnUserSID = "UserSID";
@@ -29,50 +42,78 @@ class netidUserSyncConfig
     public static $sqlColumnWPEventsComplete = "WPEventsComplete";
     public static $sqlColumnWPStudentWebComplete = "WPStudentWebComplete";
 
-    //query to get update www complete flag in netid database.
+    /**
+     * Query to get update www complete flag in netid database.
+     * @static
+     */
     public static $netIDWWWFlagUpdateQuery = <<<EOS
 UPDATE statement
 EOS;
 
-//query to get update commons complete flag in netid database.
+    /**
+     * Query to get update commons complete flag in netid database.
+     * @static
+     */
     public static $netIDCommonsFlagUpdateQuery = <<<EOS
         UPDATE statement
 EOS;
-//query to get update events complete flag in netid database.
+
+    /**
+     * Query to get update events complete flag in netid database.
+     * @static
+     */
     public static $netIDEventsFlagUpdateQuery = <<<EOS
         UPDATE statement
 EOS;
 
-//query to get update student web complete flag in netid database.
+    /**
+     * Query to get update student web complete flag in netid database.
+     * @static
+     */
     public static $netIDStudentWebFlagUpdateQuery = <<<EOS
         UPDATE statement
 EOS;
 
-//query to get usernames for main site or www complete flag is set to 0 in netid database.
-
+    /**
+     * Query to get usernames for main site or www complete flag is set to 0 in netid database.
+     * @static
+     */
     public static $getUsernamesForMain= <<<EOS
        SELECT statement
 EOS;
-    //query to get usernames for commons complete flag is set to 0 in netid database.
+
+    /**
+     * Query to get usernames for commons complete flag is set to 0 in netid database.
+     * @static
+     */
     public static $getUsernamesForCommons= <<<EOS
        SELECT statement
 EOS;
-//query to get usernames for Events complete flag is set to 0 in netid database.
+
+    /**
+     * Query to get usernames for Events complete flag is set to 0 in netid database.
+     * @static
+     */
     public static $getUsernamesForEvents= <<<EOS
        SELECT statement
 EOS;
-//query to get usernames for student web complete flag is set to 0 in netid database.
+
+    /**
+     * Query to get usernames for student web complete flag is set to 0 in netid database.
+     * @static
+     */
     public static $getUsernamesForStudentWeb= <<<EOS
        SELECT statement
 EOS;
 
-
-    public static $application_name = "";// Application the plugin is running on. Options available - main, commons, events, studentweb
+    /**
+     * Application the plugin is running on
+     *
+     * Options available - main, commons, events, studentweb
+     * @static
+     */
+    public static $application_name = "";
 
     public static $logs = false;
     public static $cronInterval = 60 ; //time in seconds;
-
 }
-
-*/
-?>
